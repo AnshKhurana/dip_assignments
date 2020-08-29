6c47279ed559961a0bad67cc37eb74e4f1f88754
kushagra <kuku12320@gmail.com>
2020-08-29 22:09:09 +0530

    corrected display format in mainScript q1

--- assignment-1-transformations/1/code/myMainScript.m
+++ assignment-1-transformations/1/code/myMainScript.m
@@ -1,78 +1,53 @@
 %% MyMainScript
 
+clc; clear all; close all;
+warning('off', 'all');
+
 tic;
 
 %% Code for Q1.a
 
 % Original circles image
-figure;
 im1 = imread("../data/circles_concentric.png");
-title("Original image");
-imshow(im1);
+displayLockedAspectRatio(im1, "Original circles image");
 
 % Subsampled by 2 circles image
-figure;
 circles2 = myShrinkImageByFactorD(im1, 2);
-title("Circles image subsampled by 2");
-imshow(circles2);
+displayLockedAspectRatio(circles2, "Circles image subsampled by 2");
 
 % Subsampled by 3 circles image
-figure;
 circles3 = myShrinkImageByFactorD(im1, 3);
-title("Circles image subsampled by 3");
-imshow(circles3);
+displayLockedAspectRatio(circles3, "Circles image subsampled by 3");
 
 toc;
 
-%% Code for Q1.b,c
+%% Code for Q1.b,c,d
 % Original barbara image
-figure;
 im2 = imread("../data/barbaraSmall.png");
-title("Original image");
-imshow(im2);
+displayLockedAspectRatio(im2, "Original image");
 
 % Currently using uint8 approximation to display the image, change later to
 % display the color map and the double image properly
-figure;
 barbaraBilinear = uint8(myBilinearInterpolation(im2, 3, 2));
-title("Bilinear Interpolated image");
-imshow(barbaraBilinear);
+displayLockedAspectRatio(barbaraBilinear, "Bilinear Interpolated image");
 
 % Nearest Neighbor Interpolation
-figure;
 barbaraNN = uint8(myNearestNeighborInterpolation(im2));
-title("Nearest Neighbor Interpolated image");
-imshow(barbaraNN);
-
-toc;
-
-%% Code for Q1.e
-% Original barbara image
-figure;
-im2 = imread("../data/barbaraSmall.png");
-title("Original image");
-imshow(im2);
+displayLockedAspectRatio(barbaraNN, "Nearest Neighbor Interpolated image");
 
 %Interpolated image
-figure;
 barbaraBicubic = uint8(myBicubicInterpolation(im2, 3, 2));
-title("Bicubic Interpolation");
-imshow(barbaraBicubic);
+displayLockedAspectRatio(barbaraBicubic, "Bicubic Interpolation");
 
 toc;
-
 
 %% Code for Q1.f
 % Original barbara image
-figure;
 im2 = imread("../data/barbaraSmall.png");
-title("Original image");
-imshow(im2);
+displayLockedAspectRatio(im2, "Original image");
 
 %Rotated image
-figure;
 barbaraRotated = uint8(myImageRotation(im2, 30));
-title("Rotated image");
-imshow(barbaraRotated);
+displayLockedAspectRatio(barbaraRotated, "Rotated image");
 
 toc;
