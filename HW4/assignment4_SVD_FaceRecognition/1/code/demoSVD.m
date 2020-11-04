0877c48d36f6f76e20bc408f82bb4f1edafad9e5
AnshKhurana <anshkhurana1507@gmail.com>
2020-11-05 00:19:37 +0530

    Q1 revised

--- HW4/assignment4_SVD_FaceRecognition/1/code/demoSVD.m
+++ HW4/assignment4_SVD_FaceRecognition/1/code/demoSVD.m
@@ -9,17 +9,28 @@ function [] = demoSVD(A)
     disp(U);
     disp('S: ')
     disp(S);
-    disp('Singular values: ')
-    n = size(S);
-    n = min(n);
-    sorted = zeros(n,1);
-    for i = 1:n
-        sorted(i) = S(i,i);
-    end
-    sorted = sort(sorted, 'descend');
-    disp(sorted');
     disp('V: ')
     disp(V);
+    %     disp('Singular values: ')
+%     n = size(S);
+%     n = min(n);
+%     sorted = zeros(n,1);
+%     for i = 1:n
+%         sorted(i) = S(i,i);
+%     end
+%     sorted = sort(sorted, 'descend');
+%     disp(sorted');
     disp("verify output by U * S * V'");
     disp(verify_A);
+%     fprintf('\n');
+%     disp("SVD computed from MATLAB function")
+%     [U_s, S_s, V_s] = svd(A);
+%     disp('U: ')
+%     disp(U_s);
+%     disp('S: ')
+%     disp(S_s);
+%     disp('V: ')
+%     disp(V_s);
+    
+    fprintf('\n\n\n');
 end
--- HW4/assignment4_SVD_FaceRecognition/1/code/myMainScript.m
+++ HW4/assignment4_SVD_FaceRecognition/1/code/myMainScript.m
@@ -12,15 +12,20 @@ tic;
 
 %% Q1: Implementation of SVD using eig
 %  demoSVD calls mySVD and shows the required values.
-
+%  Explanation of the method is given as comments in mySVD().
+%  Running some examples for verififcation of algorithm:
 
 disp('example 1')
 A = eye(5);
 demoSVD(A);
 disp('example 2')
-A = eye(3,4);
-demoSVD(A);
-disp('example 3')
 A = [ 2 3 5 7 ; 1 2 3 4 ; 1 3 4 2];
 demoSVD(A);
+disp('example 3')
+A = [ 12 3 5 7 ; 1 2 3 4 ; 8 3 4 2];
+demoSVD(A);
+disp('example 4')
+A = [ 1 2 3 4 5 6 ; 7 8 9 10 11 12; 13 14 15 16 17 18; 19 20 21 22 23 24];
+demoSVD(A);
+
 toc;
